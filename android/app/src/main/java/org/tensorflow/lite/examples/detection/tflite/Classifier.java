855e2195e0492b6eed2d117b2a5c00e49df25dea
karla-jc <karla10yacely@gmail.com>
2023-02-22 16:36:01 -0500

    aplicacion_movil

---
+++ android/app/src/main/java/org/tensorflow/lite/examples/detection/tflite/Classifier.java
@@ -0,0 +1,139 @@
+/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+package org.tensorflow.lite.examples.detection.tflite;
+
+import android.graphics.Bitmap;
+import android.graphics.RectF;
+
+import java.util.List;
+
+/**
+ * Generic interface for interacting with different recognition engines.
+ */
+public interface Classifier {
+    List<Recognition> recognizeImage(Bitmap bitmap);
+
+    void enableStatLogging(final boolean debug);
+
+    String getStatString();
+
+    void close();
+
+    void setNumThreads(int num_threads);
+
+    void setUseNNAPI(boolean isChecked);
+
+    abstract float getObjThresh();
+
+    /**
+     * Resultado inmutable devuelto por un clasificador que describe lo que se ha
+     * reconocido.
+     */
+    public class Recognition {
+        /**
+         * Un identificador único para lo que ha sido reconocido. Específico de la
+         * clase, no de la instancia de
+         * el objeto.
+         */
+        private final String id;
+
+        /**
+         * Nombre para mostrar el reconocimiento.
+         */
+        private final String title;
+
+        /**
+         * Una puntuación ordenable que indica lo bueno que es el reconocimiento en
+         * comparación con otros. Cuanto más alto, mejor.
+         */
+        private final Float confidence;
+
+        /**
+         * Ubicación opcional dentro de la imagen de origen para la localización del
+         * objeto reconocido.
+         */
+        private RectF location;
+
+        private int detectedClass;
+
+        public Recognition(
+                final String id, final String title, final Float confidence, final RectF location) {
+            this.id = id;
+            this.title = title;
+            this.confidence = confidence;
+            this.location = location;
+        }
+
+        public Recognition(final String id, final String title, final Float confidence, final RectF location,
+                int detectedClass) {
+            this.id = id;
+            this.title = title;
+            this.confidence = confidence;
+            this.location = location;
+            this.detectedClass = detectedClass;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public Float getConfidence() {
+            return confidence;
+        }
+
+        public RectF getLocation() {
+            return new RectF(location);
+        }
+
+        public void setLocation(RectF location) {
+            this.location = location;
+        }
+
+        public int getDetectedClass() {
+            return detectedClass;
+        }
+
+        public void setDetectedClass(int detectedClass) {
+            this.detectedClass = detectedClass;
+        }
+
+        @Override
+        public String toString() {
+            String resultString = "";
+            if (id != null) {
+                resultString += "[" + id + "] ";
+            }
+
+            if (title != null) {
+                resultString += title + " ";
+            }
+
+            if (confidence != null) {
+                resultString += String.format("(%.1f%%) ", confidence * 100.0f);
+            }
+
+            if (location != null) {
+                resultString += location + " ";
+            }
+
+            return resultString.trim();
+        }
+    }
+}
